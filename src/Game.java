2bb6f9782b08f595ea82035560892460d0e8a3f6
BT_2N2_31 <gjdkfk@bths.edu>
2024-03-14 13:56:06 -0400

    making stations

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Game {
+    public Game() {
+        Start();
+    }
+    public void Start() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the..");
+        System.out.println("Do you need a tutorial? (y/n)");
+        String ans = scan.nextLine().toLowerCase();
+        if(ans.equals("y")) {
+
+        }
+    }
+    public void Station1() {
+
+    }
+    public void Station2() {
+    }
+    public void Station3() {
+
+    }
+    public void Station4() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] same = new String[9][20];
+        for(int i=0; i<same.length; i++) {
+            for(int j=0; j<same[0].length; j++) {
+                same[i][j]="h";
+            }
+        }
+        for(int j=0; j<same[0].length; j++) {
+            if(j<=5) {
+                same[2][j]="-";
+            }
+            if(j==5) {
+                int k=3;
+                while(k<same.length) {
+                    same[k][j]="|";
+                    k++;
+                }
+            }
+            same[0][j]="-";
+            same[same.length-1][j]="-";
+        }
+        for(int j=0; j<same.length; j++) {
+            same[j][same[0].length-1]="|";
+
+        }
+        for(int i=0; i<same.length; i++) {
+            for(int j=0; j<same[0].length; j++) {
+                System.out.print(same[i][j]);
+            }
+            System.out.println();
+        }
+        // Game g = new Game();
+
+    }
+}
